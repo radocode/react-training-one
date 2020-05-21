acfcfa22af0723a44ff320e621a35062cfef556f
Francisco González <franciscogonzalez@MacBook-Pro.local>
2020-05-21 19:51:55 -0400

    cleanup1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-// Import React y ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-
-// Crear un componente React
-const App = () => {
-    return (
-        <div>
-            <label className="label" htmlFor="name">
-                Enter name:
-            </label>
-            <input type="text" id="name"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
-        </div>
-    );
-};
-
-// Mostrar component
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
